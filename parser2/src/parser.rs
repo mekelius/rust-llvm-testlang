6f146da529e0418c90da31e226c1820e51006954
Mihkal Jouste <mihkal.jouste@gmail.com>
2026-06-12 00:58:42 +0300

    Rename Expr to Node

--- parser2/src/parser.rs
+++ parser2/src/parser.rs
@@ -3,64 +3,66 @@ use crate::lexer::TokenData;
 use chumsky::prelude::*;
 
 #[derive(Debug, Clone)]
-pub enum Expr {
-    Program(Vec<Expr>),
-    Function((Box<Expr>, Box<Expr>, Box<Expr>)),
-    Block(Vec<Expr>),
-    Expression(Vec<Expr>),
+pub enum Node {
+    Program(Vec<Node>),
+    Function((Box<Node>, Box<Node>, Box<Node>)),
+    Block(Vec<Node>),
+    Expression(Vec<Node>),
     Identifier(String),
-    Formals(Vec<Expr>),
+    Formals(Vec<Node>),
     Formal(String),
-    FunctionBody(Vec<Expr>),
+    FunctionBody(Vec<Node>),
     While {
-        condition: Box<Expr>,
-        body: Box<Expr>,
+        condition: Box<Node>,
+        body: Box<Node>,
     },
     EmptyStatement,
-    ArgumentList(Vec<Expr>),
+    ArgumentList(Vec<Node>),
     FunctionCall {
         callee: String,
-        argument_list: Vec<Expr>,
+        argument_list: Vec<Node>,
     },
     UnaryOperator {
-        operator: Box<Expr>,
-        rhs: Box<Expr>,
+        operator: Box<Node>,
+        rhs: Box<Node>,
     },
     UnaryMinus,
 }
 
 fn parenthesized<'src>(
-    a: impl Parser<'src, &'src [Token], Expr, chumsky::extra::Err<Rich<'src, Token>>> + Clone,
-) -> impl Parser<'src, &'src [Token], Expr, chumsky::extra::Err<Rich<'src, Token>>> + Clone {
+    a: impl Parser<'src, &'src [Token], Node, chumsky::extra::Err<Rich<'src, Token>>> + Clone,
+) -> impl Parser<'src, &'src [Token], Node, chumsky::extra::Err<Rich<'src, Token>>> + Clone {
     a.delimited_by(just(Token::LParenthesis), just(Token::RParenthesis))
 }
 
 fn in_curly_braces<'src>(
-    a: impl Parser<'src, &'src [Token], Expr, chumsky::extra::Err<Rich<'src, Token>>> + Clone,
-) -> impl Parser<'src, &'src [Token], Expr, chumsky::extra::Err<Rich<'src, Token>>> + Clone {
+    a: impl Parser<'src, &'src [Token], Node, chumsky::extra::Err<Rich<'src, Token>>> + Clone,
+) -> impl Parser<'src, &'src [Token], Node, chumsky::extra::Err<Rich<'src, Token>>> + Clone {
     a.delimited_by(just(Token::LCurlyBrace), just(Token::RCurlyBrace))
 }
 
 pub fn parser<'src>()
--> impl Parser<'src, &'src [Token], Expr, chumsky::extra::Err<Rich<'src, Token>>> + Clone {
+-> impl Parser<'src, &'src [Token], Node, chumsky::extra::Err<Rich<'src, Token>>> + Clone {
     let identifier = select! {
-        Token::Identifier(TokenData {value}) => Expr::Identifier(value),
+        Token::Identifier(TokenData {value}) => Node::Identifier(value),
     };
 
-    let unary_operator = just(Token::Minus).to(Expr::UnaryMinus);
+    let unary_operator = just(Token::Minus).to(Node::UnaryMinus);
+
+    // let binary_operator = just(Token::Minus).to(Node::UnaryMinus);
 
     let expression = recursive(|expr| {
         let argument_list = parenthesized(
             expr.clone()
                 .separated_by(just(Token::Comma))
-                .collect::<Vec<Expr>>()
-                .map(|e| Expr::ArgumentList(e)),
+                .collect::<Vec<Node>>()
+                .map(|e| Node::ArgumentList(e)),
         );
 
         let unary_expression =
             unary_operator
                 .then(expr.clone())
-                .map(|(operator, rhs)| Expr::UnaryOperator {
+                .map(|(operator, rhs)| Node::UnaryOperator {
                     operator: Box::new(operator),
                     rhs: Box::new(rhs),
                 });
@@ -68,7 +70,7 @@ pub fn parser<'src>()
         // let binary_expression = todo();
 
         // let function_call = identifier.then(argument_list).map(
-        //     |(Expr::Identifier(value), Expr::ArgumentList(argument_list))| Expr::FunctionCall {
+        //     |(Node::Identifier(value), Node::ArgumentList(argument_list))| Node::FunctionCall {
         //         callee: value,
         //         argument_list,
         //     },
@@ -78,16 +80,16 @@ pub fn parser<'src>()
             .then(argument_list)
             .map(|(identifier, arguments)| {
                 let callee = match identifier {
-                    Expr::Identifier(value) => value,
+                    Node::Identifier(value) => value,
                     _ => todo!(),
                 };
 
                 let argument_list = match arguments {
-                    Expr::ArgumentList(argument_list) => argument_list,
+                    Node::ArgumentList(argument_list) => argument_list,
                     _ => todo!(),
                 };
 
-                Expr::FunctionCall {
+                Node::FunctionCall {
                     callee,
                     argument_list,
                 }
@@ -108,18 +110,18 @@ pub fn parser<'src>()
         let while_ = just(Token::While)
             .ignore_then(parenthesized(expression.clone()))
             .then(p.clone())
-            .map(|(condition, body)| Expr::While {
+            .map(|(condition, body)| Node::While {
                 condition: Box::new(condition),
                 body: Box::new(body),
             });
 
-        let empty_statement = empty().to(Expr::EmptyStatement);
+        let empty_statement = empty().to(Node::EmptyStatement);
 
         let block = in_curly_braces(
             p.clone()
                 .repeated()
-                .collect::<Vec<Expr>>()
-                .map(|e| Expr::Block(e)),
+                .collect::<Vec<Node>>()
+                .map(|e| Node::Block(e)),
         );
 
         let single_statement =
@@ -131,21 +133,21 @@ pub fn parser<'src>()
     });
 
     let formal = select! {
-        Token::Identifier(TokenData {value}) => Expr::Formal(value)
+        Token::Identifier(TokenData {value}) => Node::Formal(value)
     };
 
     let function_body = in_curly_braces(
         statement
             .repeated()
-            .collect::<Vec<Expr>>()
-            .map(|e| Expr::FunctionBody(e)),
+            .collect::<Vec<Node>>()
+            .map(|e| Node::FunctionBody(e)),
     );
 
     let formals = parenthesized(
         formal
             .separated_by(just(Token::Comma))
-            .collect::<Vec<Expr>>()
-            .map(|e| Expr::Formals(e)),
+            .collect::<Vec<Node>>()
+            .map(|e| Node::Formals(e)),
     );
 
     let function = just(Token::Function)
@@ -153,13 +155,13 @@ pub fn parser<'src>()
         .then(formals)
         .then(function_body)
         .map(|((name, formals), function_body)| {
-            Expr::Function((Box::new(name), Box::new(formals), Box::new(function_body)))
+            Node::Function((Box::new(name), Box::new(formals), Box::new(function_body)))
         });
 
     let program = function
         .repeated()
-        .collect::<Vec<Expr>>()
-        .map(|e| Expr::Program(e));
+        .collect::<Vec<Node>>()
+        .map(|e| Node::Program(e));
 
     program
 }
