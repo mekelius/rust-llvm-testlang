6c271a33bac0baaefc4ff9fc9f126a98c4347f2c
Mihkal Jouste <mihkal.jouste@gmail.com>
2026-06-21 21:42:16 +0300

    Add for loops

--- test_lang/src/codegen/loop.rs
+++ test_lang/src/codegen/loop.rs
@@ -1,3 +1,8 @@
+use inkwell::{
+    types::BasicTypeEnum,
+    values::{AnyValue, BasicValueEnum},
+};
+
 use super::CodeGen;
 use crate::ast::Node;
 
@@ -46,7 +51,79 @@ impl<'ctx> CodeGen<'ctx> {
         self.ir.builder.position_at_end(after_block);
     }
 
-    pub fn handle_for(&self, _init: &Node, _condition: &Node, _step: &Node, _body: &Node) {
-        todo!("For loops");
+    pub fn handle_for(&mut self, init: &Node, condition: &Node, step: &Node, body: &Node) {        
+        let from_block = self.ir.builder.get_insert_block().unwrap();
+        let current_function = from_block.get_parent().unwrap();
+
+        self.scopes.push_new_scope();
+
+        let header_block = self
+            .ir
+            .context
+            .append_basic_block(current_function, "loop_condition");
+        let body_block = self
+            .ir
+            .context
+            .append_basic_block(current_function, "loop_body");
+        let step_block = self
+            .ir
+            .context
+            .append_basic_block(current_function, "loop_step");
+        let after_block = self
+            .ir
+            .context
+            .append_basic_block(current_function, "loop_after");
+
+        // Loop init
+        self.handle_statement(init);
+        let condition_value_initial = self.handle_expression(condition);
+        self.ir
+            .builder
+            .build_unconditional_branch(header_block)
+            .unwrap();
+
+        // Loop step
+        self.ir.builder.position_at_end(step_block);
+        self.handle_statement(step);
+        let condition_value_updated = self.handle_expression(condition);
+        self.ir
+            .builder
+            .build_unconditional_branch(header_block)
+            .unwrap();
+
+        // Loop header
+        self.ir.builder.position_at_end(header_block);
+
+        let condition_value_initial: BasicValueEnum<'ctx> =
+            condition_value_initial.try_into().unwrap();
+        let condition_value_updated: BasicValueEnum<'ctx> =
+            condition_value_updated.try_into().unwrap();
+        let phi_type: BasicTypeEnum<'ctx> = condition_value_initial.get_type().try_into().unwrap();
+        let phi = self
+            .ir
+            .builder
+            .build_phi(phi_type, "for_loop_condition")
+            .unwrap();
+        phi.add_incoming(&[
+            (&condition_value_initial, from_block),
+            (&condition_value_updated, step_block),
+        ]);
+        self.ir
+            .builder
+            .build_conditional_branch(
+                phi.as_any_value_enum().into_int_value(),
+                body_block,
+                after_block,
+            )
+            .unwrap();
+
+        // Loop body
+        self.ir.builder.position_at_end(body_block);
+        self.handle_statement(body);
+        self.ir.builder.build_unconditional_branch(step_block).unwrap();
+
+        // clean up
+        self.ir.builder.position_at_end(after_block);
+        self.scopes.pop_scope();
     }
 }
